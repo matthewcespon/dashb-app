1d68d82c6f4d6c0a638edbc60fa38176cfac65eb
Matthew Cespon <98528639+matthewcespon@users.noreply.github.com>
2025-03-23 22:17:42 +0000

    loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import styles from './loading.module.css';
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className={styles.container}>
+        <div className={styles.line}></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/loading/page.tsx
@@ -0,0 +1,7 @@
+import Loading from '@/app/loading';
+
+export default function LoadingTest() {
+  return (
+    <Loading />
+  );
+}
