99ead57a6ff269ba863b64bde0835694191e993a
Matthew Cespon <mattces1@yahoo.com>
2025-03-31 22:43:18 +0100

    reprots

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -4,6 +4,7 @@ import { getReports } from "@/app/actions/reports";
 export default async function Home() {
   const response = await getReports();
   const reports = response?.reports || [];
+  console.log(reports)
   
   return (
     <div className="space-y-4">
