1c260b5f4eb5ec9570547abf1332b141ae704e26
Matthew Cespon <mattces1@yahoo.com>
2025-04-09 19:45:19 +0300

    debug

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { ReportsTable } from "@/components/reports-table";
-import { getReports } from "@/app/actions/reports";
-import { Report } from "@/app/dashboard/reports";
+import { useState, useEffect } from 'react'
+import { ReportsTable } from "@/components/reports-table"
+import { getReports } from "@/app/actions/reports"
+import { Report } from "@/app/dashboard/reports"
 
 export default function Home() {
-  const [reports, setReports] = useState<Report[]>([]);
-  const [token, setToken] = useState<string | null>(null);
+  const [reports, setReports] = useState<Report[]>([])
+  const [token, setToken] = useState<string | null>(null)
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    setToken(storedToken);
+    const storedToken = localStorage.getItem('token')
+    console.log("storedToken", storedToken)
+    setToken(storedToken)
   }, []);
 
   useEffect(() => {
     async function fetchReports() {
       if (token) {
+        console.log("Fetching reports with token:", token)
         const data = await getReports(token);
+        console.log("Fetched reports:", data)
         setReports(data);
       }
     }
